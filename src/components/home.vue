8eb10090e041d1c1df6306a7322e45f853c76103
feng_q <feng_q@worksap.co.jp>
2019-05-23 14:16:07 +0800

    introduce elementUI to beautify the page

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,25 +1,50 @@
 <template>
   <div>
-    <label for="gene-data">Choose the gene data file:</label>
-    <input
-      type="file"
-      id="gene-data"
-      name="geneData"
-      accept=".txt"
-      @change="uploadGeneData"
-    >
-
-    <label for="gene-data-preview">Content Preview:</label>
-    <textarea id="gene-data-preview">{{ previewText }}</textarea><br />
-
-    <button
-      class="submitbutton"
-      :disabled="disabled"
-      @click="handleSubmitGeneData"
-    >Process</button>
-
-    <label for="gene-data-result">Result:</label>
-    <textarea id="gene-data-result">{{ outputText }}</textarea>
+    <el-row type="flex" justify="center">
+      <el-upload
+        id="upload-gene-file"
+        class="upload-gene-file"
+        drag
+        action=""
+        :on-change="uploadGeneData"
+        :auto-upload=false
+      >
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">
+          Drop file here or
+          <em>click to upload</em>
+        </div>
+        <div class="el-upload__tip" slot="tip">txt file with no more than 50 sequences</div>
+      </el-upload>
+    </el-row>
+    <el-row type="flex" justify="center">
+      <el-button
+        type="default"
+        class="submit-button"
+        :disabled="disabled"
+        @click="handleSubmitGeneData"
+        icon="el-icon-magic-stick"
+        round
+      >Process</el-button>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-row>
+          <p class="gene-label">Content Preview:</p>
+        </el-row>
+        <el-row>
+          <el-input id="preview-text" class="preview-text" type="textarea" v-model="previewText"></el-input>
+        </el-row>
+      </el-col>
+      <el-col :span="12">
+        <el-row>
+          <p class="gene-label">Result:</p>
+        </el-row>
+        <el-row>
+          <el-input id="result-text" class="result-text" type="textarea" v-model="outputText"></el-input>
+        </el-row>
+      </el-col>
+    </el-row>
   </div>
 </template>
 <script>
@@ -28,35 +53,35 @@ export default {
     return {
       disabled: this.$store.state.disabled,
       previewText: this.$store.state.previewText,
-      outputText: this.$store.state.outputText
+      outputText: this.$store.state.outputText,
+      uploadForm: new FormData()
     };
   },
   methods: {
-    uploadGeneData(event) {
-      let formData = new FormData();
-      let uploadFile = event.target.files[0];
-      formData.append("file", uploadFile);
-      let url = "/api/upload";
-      let config = { headers: { "Content-Type": "multipart/form-data" } };
-      this.axios.post(url, formData, config).then(response => {
-        console.log(response.data);
-        let _this = this;
+    uploadGeneData(file) {
+      this.disabled = false;
+      this.outputText = "";
 
-        let reader = new FileReader();
+      let uploadFile = file.raw;
+      let reader = new FileReader();
+      let _this = this;
 
-        reader.onload = function(e) {
-          _this.previewText = e.target.result;
-          _this.outputText = "";
-          _this.disabled = false;
-        };
+      reader.onload = function(e) {
+        _this.previewText = e.target.result;
+      };
 
-        reader.readAsText(uploadFile);
-      });
+      reader.readAsText(uploadFile);
+
+      this.uploadForm.set('file', file.raw);
     },
 
     handleSubmitGeneData(event) {
-      let url = "/api/handle";
-      this.axios.get(url).then(response => {
+      let url = "/api/upload";
+      let config = { headers: { "Content-Type": "multipart/form-data" } };
+
+      let formData = this.uploadForm
+
+      this.axios.post(url, formData, config).then(response => {
         console.log(response.data);
         this.outputText = response.data;
         this.disabled = true;
@@ -68,17 +93,8 @@ export default {
 
 
 <style>
-label {
-  display: block;
-  font: 1rem "Fira Sans", sans-serif;
-}
-
-input,
-label {
-  margin: 0.4rem 0;
-}
-
-.note {
-  font-size: 0.8em;
+.gene-label {
+  color: #606266;
+  font-size: 14px;
 }
 </style>
